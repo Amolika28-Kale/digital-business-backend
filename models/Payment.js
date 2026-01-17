f02f338011dd9387fcc45ce43af7e29a9c80ff3e
Amolika28-Kale <amolikakale234@gmail.com>
2026-01-17 16:15:59 +0530

    Payment Model change

--- models/Payment.js
+++ models/Payment.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
-const paymentSchema = new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  stripeSessionId: String,
-  amount: Number,
-  status: String,
-}, { timestamps: true });
+const paymentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // 🔥 THIS IS THE FIX
+      required: true,
+    },
+    stripeSessionId: String,
+    amount: Number,
+    status: String,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Payment", paymentSchema);
